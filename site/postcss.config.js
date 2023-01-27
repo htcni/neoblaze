bd04d15b76fd6d1d3341059dabf87ab8a81a396d
Nitin Bartwal <nitinbartwal6@gmail.com>
2023-01-27 21:37:10 +0530

    chore: prettier format

--- site/postcss.config.js
+++ site/postcss.config.js
@@ -1,9 +1,9 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
+const purgecss = require('@fullhuman/postcss-purgecss');
 module.exports = {
 	plugins: [
-		require("autoprefixer"),
+		require('autoprefixer'),
 		purgecss({
-			content: ["./layouts/**/*.html"],
+			content: ['./layouts/**/*.html'],
 		}),
 	],
 };
