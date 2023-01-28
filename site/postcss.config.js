663a60bdb594fc271ea194bed7e9a2255a16d24b
Nitin Bartwal <nitinbartwal6@gmail.com>
2023-01-28 12:44:42 +0530

    site: add hugo purge config

--- site/postcss.config.js
+++ site/postcss.config.js
@@ -3,7 +3,16 @@ module.exports = {
 	plugins: [
 		require('autoprefixer'),
 		purgecss({
-			content: ['./layouts/**/*.html'],
+			content: ['./hugo_stats.json'],
+			defaultExtractor: (content) => {
+				const els = JSON.parse(content).htmlElements;
+				return [
+					...(els.tags || []),
+					...(els.classes || []),
+					...(els.ids || []),
+				];
+			},
+			safelist: [],
 		}),
 	],
 };
